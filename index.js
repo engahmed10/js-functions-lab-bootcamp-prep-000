dad7a943852b58c5b524af12a614b8fb494702c9
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-12 08:18:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 // write your code below!
-var date = "July 3rd"
-console.log("My birthday is " + date)
-onsole.log(`My birthday is ${date}`)
-console.log(`I have ${1 + 1} pets`)
-console.log("I have " + 1 + 1 + " pets")
+
 ///########################
 function happyHolidays()
 {
